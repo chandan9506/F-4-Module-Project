8faf75150e92e50d8dde3564b1c42395e5977621
Chandan <chandan.ckmodi@gmail.com>
2024-09-05 21:53:26 +0530

    First commit

---
+++ src/Components/SortButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SortButton = ({ onClick, label }) => {
+    return (
+        <button onClick={onClick}>
+            {label}
+        </button>
+    );
+};
+
+export default SortButton;
+
+
